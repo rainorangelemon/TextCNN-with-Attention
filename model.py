1e01c85f87e441236ddea09fbbecd00c1ecb6804
rainorangelemon <rainorangelemon@gmail.com>
2019-11-29 22:44:38 -0800

    finish basic modification on textCNN

--- model.py
+++ model.py
@@ -11,12 +11,139 @@ import os
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import math
+import copy
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, heads, d_model, dropout=0.1):
+        super().__init__()
+
+        self.d_model = d_model
+        self.d_k = d_model // heads
+        self.h = heads
+
+        self.q_linear = nn.Linear(d_model, d_model)
+        self.v_linear = nn.Linear(d_model, d_model)
+        self.k_linear = nn.Linear(d_model, d_model)
+        self.dropout = nn.Dropout(dropout)
+        self.out = nn.Linear(d_model, d_model)
+
+    def forward(self, q, k, v, mask=None):
+        bs = q.size(0)
+
+        # perform linear operation and split into h heads
+
+        k = self.k_linear(k).view(bs, -1, self.h, self.d_k)
+        q = self.q_linear(q).view(bs, -1, self.h, self.d_k)
+        v = self.v_linear(v).view(bs, -1, self.h, self.d_k)
+
+        # transpose to get dimensions bs * h * sl * d_model
+
+        k = k.transpose(1, 2)
+        q = q.transpose(1, 2)
+        v = v.transpose(1, 2)  # calculate attention using function we will define next
+        scores = self.attention(q, k, v, self.d_k, mask, self.dropout)
+
+        # concatenate heads and put through final linear layer
+        concat = scores.transpose(1, 2).contiguous() \
+            .view(bs, -1, self.d_model)
+
+        output = self.out(concat)
+
+        return output
+
+    def attention(self, q, k, v, d_k, mask=None, dropout=None):
+
+        scores = torch.matmul(q, k.transpose(-2, -1)) / math.sqrt(d_k)
+        if mask is not None:
+            mask = mask.unsqueeze(1)
+            scores = scores.masked_fill(mask == 0, -1e9)
+            scores = F.softmax(scores, dim=-1)
+
+        if dropout is not None:
+            scores = dropout(scores)
+
+        output = torch.matmul(scores, v)
+        return output
+
+
+class FeedForward(nn.Module):
+    def __init__(self, d_model, d_ff=4, dropout = 0.1):
+        super().__init__()
+        # We set d_ff as a default to 2048
+        self.linear_1 = nn.Linear(d_model, d_ff)
+        self.dropout = nn.Dropout(dropout)
+        self.linear_2 = nn.Linear(d_ff, d_model)
+
+    def forward(self, x):
+        x = self.dropout(F.relu(self.linear_1(x)))
+        x = self.linear_2(x)
+        return x
+
+
+class Norm(nn.Module):
+    def __init__(self, d_model, eps=1e-6):
+        super().__init__()
+
+        self.size = d_model
+        # create two learnable parameters to calibrate normalisation
+        self.alpha = nn.Parameter(torch.ones(self.size), requires_grad=True)
+        self.bias = nn.Parameter(torch.zeros(self.size), requires_grad=True)
+        self.eps = eps
+
+    def forward(self, x):
+        norm = self.alpha * (x - x.mean(dim=-1, keepdim=True)) \
+               / (x.std(dim=-1, keepdim=True) + self.eps) + self.bias
+        return norm
+
+
+class EncoderLayer(nn.Module):
+    def __init__(self, d_model, heads, dropout=0.1):
+        super().__init__()
+        self.norm_1 = Norm(d_model)
+        self.norm_2 = Norm(d_model)
+        self.attn = MultiHeadAttention(heads, d_model)
+        self.ff = FeedForward(d_model)
+        self.dropout_1 = nn.Dropout(dropout)
+        self.dropout_2 = nn.Dropout(dropout)
+
+    def forward(self, x, mask):
+        x2 = self.norm_1(x)
+        x = x + self.dropout_1(self.attn(x2, x2, x2, mask))
+        x2 = self.norm_2(x)
+        x = x + self.dropout_2(self.ff(x2))
+        return x
+
+
+def get_clones(module, N=1):
+    return nn.ModuleList([copy.deepcopy(module) for i in range(N)])
+
+
+def create_mask(input, pad_id):
+    input_msk = (input != pad_id).unsqueeze(1)
+    return input_msk
+
+
+class SentenceEncoder(nn.Module):
+    def __init__(self, d_model, N, heads):
+        super().__init__()
+        self.N = N
+        self.layers = get_clones(EncoderLayer(d_model, heads), N)
+        self.norm = Norm(d_model)
+
+    def forward(self, x, mask):
+        for i in range(self.N):
+            x = (self.layers[i])(x, mask)
+        x = self.norm(x)
+        return x
 
 
 class TextCNN(nn.Module):
-    def __init__(self, args):
+    def __init__(self, args, pad_id):
         super(TextCNN, self).__init__()
         self.args = args
+        self.pad_id = pad_id
 
         V = args.embed_num
         D = args.embed_dim
@@ -33,6 +160,7 @@ class TextCNN(nn.Module):
         self.conv15 = nn.Conv2d(Ci, Co, (5, D))
         '''
         self.dropout = nn.Dropout(args.dropout)
+        self.sentence_encoder = SentenceEncoder(len(Ks) * Co, 1, 3)
         self.fc1 = nn.Linear(len(Ks) * Co, C)
 
     def conv_and_pool(self, x, conv):
@@ -41,6 +169,8 @@ class TextCNN(nn.Module):
         return x
 
     def forward(self, x):
+        mask = create_mask(x,self.pad_id)
+
         x = self.embed(x)  # (N, W, D)
         x = x.unsqueeze(1)  # (N, Ci, W, D)
 
@@ -59,6 +189,7 @@ class TextCNN(nn.Module):
         x = torch.cat((x1, x2, x3), 1) # (N,len(Ks)*Co)
         '''
         x = self.dropout(x)  # (N, len(Ks)*Co)
+        x = self.sentence_encoder(x, mask)
         logit = self.fc1(x)  # (N, C)
         return logit
 
